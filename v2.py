098ed7638be9bc7ecec81941b1901a3257925cb7
Mauro Pisu <pisu.maru@gmail.com>
2016-04-03 11:44:37 +0100

    Update v2.py

--- v2.py
+++ v2.py
@@ -8,15 +8,14 @@ status = 0
 
 def initStatus():
 	global status
+	subprocess.call("cls",shell=True)
+	print("\tTRANSPOSE MUSIC!!\n\n")
 	if status  == 0:
-		print("Welcome, press enter to start or esc to exit")
-	
-	elif status  == 1:
-		print("status {}".format(status))
-	elif status  == 2:
-		print("status {}".format(status))
-	elif status  == 3:
+		print("Welcome, press enter to start or esc to exit")	
+	elif status  == numStatus-1:
 		print("Process finished, press enter to close the application")
+	else:
+		print("status {}".format(status))
 
 def manageEnter():
 	global status
@@ -40,38 +39,44 @@ def manageEsc():
 	
 def manageSpacebar():
 	global status
+	if status == 0 or status == numStatus-1:
+		return False
 	print("space")
 	return False
 	
 def manageDownArrow():
 	global status
+	if status == 0 or status == numStatus-1:
+		return False
 	print("down")
 	return False
 	
 def manageUpArrow():
 	global status
+	if status == 0 or status == numStatus-1:
+		return False
 	print("up")
 	return False
 	
 def manageLeftArrow():
 	global status
+	if status == 0 or status == numStatus-1:
+		return False
 	print("left")
 	return False
 	
 def manageRightArrow():
 	global status
+	if status == 0 or status == numStatus-1:
+		return False
 	print("right")
 	return False
 
-subprocess.call("cls",shell=True)
-print("\tTRANSPOSE MUSIC!!\n\n")
 initStatus()
 	
 done = False
 while not done:
 	key = ord(msvcrt.getch())
-	subprocess.call("cls",shell=True)
-	print("\tTRANSPOSE MUSIC!!\n\n")
 	if key == 27: #esc
 		done = manageEsc()
 	elif key == 72: #up arrow
